aaa47ae157201cd0492e567316086cb0d009fb4b
dldyou <chan12_yun@naver.com>
2026-06-16 09:51:00 +0900

    ✨ feat: initialize server and handle server_fd setup in main function

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,12 @@
 #include <iostream>
 
 #include "config/Config.hpp"
+#include "server.hpp"
 
 int main(void) {
     Config config;
+    Server server;
+    int server_fd = -1;
 
     if (!config.load("config/config.ini")) {
         std::cerr << "Failed to load config.ini\n";
@@ -18,6 +21,12 @@ int main(void) {
     std::cout << "port: " << server_config.port << "\n";
     std::cout << "max_clients: " << server_config.max_clients << "\n";
     std::cout << "buffer_size: " << server_config.buffer_size << "\n";
+
+    if ((server_fd = server.init(server_config)) < 0) {
+        std::cerr << "server_fd: " << server_fd << "\n";
+        return 0;
+    }
+    server.run(server_fd, server_config);
 
     return 0;
 }
