b2cd8354c2a631d19e7d79ca2b1e7b55bfbe6b61
dldyou <chan12_yun@naver.com>
2026-06-17 10:36:19 +0900

    🎨 refactor: remove redundant 'void' specifier in function declarations

--- src/Server.hpp
+++ src/Server.hpp
@@ -14,12 +14,12 @@ private:
     int wake_fd = -1;
     std::mutex wake_mutex;
 
-    void initClients(void);
+    void initClients();
     bool addClient(int client_fd);
     bool removeClient(int client_fd);
     bool setNonBlocking(int fd);
     void closeClient(int epoll_fd, int client_fd);
-    void cleanupClients(void);
+    void cleanupClients();
 public:
     int init(const ServerConfig& server_config);
     void run(int server_fd, const ServerConfig& server_config);
