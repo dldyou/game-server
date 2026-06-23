2cceeeecf7b20cac7b98f69e54edc30b07ffce2b
dldyou <chan12_yun@naver.com>
2026-06-23 23:05:17 +0900

    ✨ feat: add UserManager class for user authentication and management

---
+++ src/users/UserManager.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+#include "User.hpp"
+#include "auth/AuthResult.hpp"
+
+class UserManager {
+private:
+    std::unordered_map<std::string, User> usersByLoginId;
+
+    bool verifyPassword(
+        std::string_view password,
+        std::string_view passwordHash
+    ) const;
+
+public:
+    bool load(const std::string& path);
+    bool createUser(User user);
+
+    AuthenticationResult authenticate(
+        std::string_view loginId,
+        std::string_view password
+    ) const;
+};
