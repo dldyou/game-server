880635e8ef4004da56039003cf9366a6928000ef
dldyou <chan12_yun@naver.com>
2026-06-30 05:29:35 +0900

    ✨ feat: impl of RoomManager

--- src/rooms/RoomManager.cpp
+++ src/rooms/RoomManager.cpp
@@ -1,25 +1,159 @@
 #include "RoomManager.hpp"
 
-RoomOperationResult RoomManager::createRoom(const AuthenticatedUser & user, int session_fd, const CreateRoomRequest & request)
-{
-    return RoomOperationResult();
+#include <utility>
+
+RoomOperationResult RoomManager::createRoom(const AuthenticatedUser& user, int session_fd, const CreateRoomRequest& request) {
+    if (user.user_id == 0) {
+        return { RoomResult::NotAuthenticated, std::nullopt };
+    }
+
+    if (request.room_name.empty() || request.max_players == 0) {
+        return { RoomResult::InvalidPayload, std::nullopt };
+    }
+
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    auto current_room_it = room_by_user_id.find(user.user_id);
+    if (current_room_it != room_by_user_id.end()) {
+        return { RoomResult::AlreadyInRoom, current_room_it->second };
+    }
+
+    const std::uint32_t room_id = next_room_id++;
+    if (next_room_id == 0) {
+        next_room_id = 1;
+    }
+
+    Room room(room_id, request.room_name, static_cast<std::size_t>(request.max_players));
+    if (!room.addPlayer({
+            .user_id = user.user_id,
+            .handle = user.handle,
+            .session_fd = session_fd,
+        })) {
+        return { RoomResult::InternalError, std::nullopt };
+    }
+
+    auto [room_it, room_inserted] = rooms.try_emplace(room_id, std::move(room));
+    if (!room_inserted) {
+        return { RoomResult::InternalError, std::nullopt };
+    }
+
+    auto [_, mapping_inserted] = room_by_user_id.try_emplace(user.user_id, room_id);
+    if (!mapping_inserted) {
+        rooms.erase(room_it);
+        return { RoomResult::InternalError, std::nullopt };
+    }
+
+    return { RoomResult::Success, room_id };
 }
 
-RoomOperationResult RoomManager::joinRoom(const AuthenticatedUser & user, int session_fd, std::uint32_t room_id)
-{
-    return RoomOperationResult();
+RoomOperationResult RoomManager::joinRoom(const AuthenticatedUser& user, int session_fd, std::uint32_t room_id) {
+    if (user.user_id == 0) {
+        return { RoomResult::NotAuthenticated, std::nullopt };
+    }
+
+    if (room_id == 0) {
+        return { RoomResult::InvalidPayload, std::nullopt };
+    }
+
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    auto current_room_it = room_by_user_id.find(user.user_id);
+    if (current_room_it != room_by_user_id.end()) {
+        return { RoomResult::AlreadyInRoom, current_room_it->second };
+    }
+
+    auto room_it = rooms.find(room_id);
+    if (room_it == rooms.end()) {
+        return { RoomResult::RoomNotFound, room_id };
+    }
+
+    Room& room = room_it->second;
+    if (room.isFull()) {
+        return { RoomResult::RoomFull, room_id };
+    }
+
+    if (!room.addPlayer({
+            .user_id = user.user_id,
+            .handle = user.handle,
+            .session_fd = session_fd,
+        })) {
+        return { RoomResult::InternalError, room_id };
+    }
+
+    auto [_, mapping_inserted] = room_by_user_id.try_emplace(user.user_id, room_id);
+    if (!mapping_inserted) {
+        room.removePlayer(user.user_id);
+        return { RoomResult::InternalError, room_id };
+    }
+
+    return { RoomResult::Success, room_id };
 }
 
-RoomOperationResult RoomManager::leaveRoom(std::uint64_t user_id)
-{
-    return RoomOperationResult();
+RoomOperationResult RoomManager::leaveRoom(std::uint64_t user_id) {
+    if (user_id == 0) {
+        return { RoomResult::NotAuthenticated, std::nullopt };
+    }
+
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    auto mapping_it = room_by_user_id.find(user_id);
+    if (mapping_it == room_by_user_id.end()) {
+        return { RoomResult::NotInRoom, std::nullopt };
+    }
+
+    const std::uint32_t room_id = mapping_it->second;
+    room_by_user_id.erase(mapping_it);
+
+    auto room_it = rooms.find(room_id);
+    if (room_it == rooms.end()) {
+        return { RoomResult::RoomNotFound, room_id };
+    }
+
+    if (!room_it->second.removePlayer(user_id)) {
+        return { RoomResult::InternalError, room_id };
+    }
+
+    if (room_it->second.empty()) {
+        rooms.erase(room_it);
+    }
+
+    return { RoomResult::Success, room_id };
 }
 
-void RoomManager::removeSession(std::uint64_t user_id)
-{
+void RoomManager::removeSession(std::uint64_t user_id) {
+    if (user_id == 0) {
+        return;
+    }
+
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    auto mapping_it = room_by_user_id.find(user_id);
+    if (mapping_it == room_by_user_id.end()) {
+        return;
+    }
+
+    const std::uint32_t room_id = mapping_it->second;
+    room_by_user_id.erase(mapping_it);
+
+    auto room_it = rooms.find(room_id);
+    if (room_it == rooms.end()) {
+        return;
+    }
+
+    room_it->second.removePlayer(user_id);
+    if (room_it->second.empty()) {
+        rooms.erase(room_it);
+    }
 }
 
-std::optional<std::uint32_t> RoomManager::roomIdOf(std::uint64_t user_id) const
-{
-    return std::optional<std::uint32_t>();
+std::optional<std::uint32_t> RoomManager::roomIdOf(std::uint64_t user_id) const {
+    std::lock_guard<std::mutex> lock(mutex_);
+
+    auto mapping_it = room_by_user_id.find(user_id);
+    if (mapping_it == room_by_user_id.end()) {
+        return std::nullopt;
+    }
+
+    return mapping_it->second;
 }
+
